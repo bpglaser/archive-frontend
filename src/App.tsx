33ca49d5bdf87a3c58abf1ace36ace2946038773
bpglaser <bpglaser@gmail.com>
2019-08-30 12:21:17 -0400

    Initial impl of login/register

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,19 @@ import { HashRouter, Route } from 'react-router-dom';
 import './App.css';
 import Primary from './Primary';
 import Navbar from './Navbar';
+import { Login, LoginDisplayMode } from './Login';
 
 type State = {
-  loggedIn: boolean
+  loggedIn: boolean,
+  loginDisplayMode: LoginDisplayMode,
 }
 
 class App extends React.Component<any, State> {
   constructor(props: any) {
     super(props)
     this.state = {
-      loggedIn: false
+      loggedIn: false,
+      loginDisplayMode: LoginDisplayMode.Hidden,
     }
   }
   render() {
@@ -25,16 +28,47 @@ class App extends React.Component<any, State> {
           logOutClicked={this.logOutClicked} />
 
         <Route path="/" exact component={Primary} />
+
+        {this.state.loginDisplayMode !== LoginDisplayMode.Hidden &&
+          <Login
+            close={this.closeLoginPrompt}
+            login={this.loginCompleted}
+            mode={this.state.loginDisplayMode}
+            register={this.registerCompleted} />
+        }
       </HashRouter>
     </div>)
   }
 
+  closeLoginPrompt = () => {
+    this.setState({
+      loginDisplayMode: LoginDisplayMode.Hidden,
+    })
+  }
+
   registerClicked = () => {
+    this.setState({
+      loginDisplayMode: LoginDisplayMode.Register,
+    })
+  }
+
+  registerCompleted = () => {
+    this.setState({
+      loginDisplayMode: LoginDisplayMode.Hidden,
+      loggedIn: true,
+    })
   }
 
   logInClicked = () => {
     this.setState({
-      loggedIn: true
+      loginDisplayMode: LoginDisplayMode.Login,
+    })
+  }
+
+  loginCompleted = () => {
+    this.setState({
+      loginDisplayMode: LoginDisplayMode.Hidden,
+      loggedIn: true,
     })
   }
 
---
+++ src/Login.tsx
@@ -0,0 +1,82 @@
+import React from "react";
+
+export enum LoginDisplayMode {
+  Hidden,
+  Login,
+  Register,
+}
+
+type Props = {
+  close: () => void,
+  login: () => void,
+  mode: LoginDisplayMode,
+  register: () => void,
+}
+
+export class Login extends React.Component<Props> {
+  render() {
+    let title = this.props.mode === LoginDisplayMode.Register ? "Register" : "Login"
+    return (<div className="modal is-active">
+      <div className="modal-background" onClick={this.props.close}></div>
+      <div className="modal-card">
+        <header className="modal-card-head">
+          <p className="modal-card-title">{title}</p>
+          <button className="delete" aria-label="close" onClick={this.props.close}></button>
+        </header>
+
+        <section className="modal-card-body">
+          <div className="field">
+            <p className="control has-icons-left">
+              <input className="input" type="email" placeholder="Email" />
+              <span className="icon is-small is-left">
+                <i className="fas fa-envelope"></i>
+              </span>
+            </p>
+          </div>
+
+          <div className="field">
+            <p className="control has-icons-left">
+              <input className="input" type="password" placeholder="Password" />
+              <span className="icon is-small is-left">
+                <i className="fas fa-lock"></i>
+              </span>
+            </p>
+          </div>
+
+          {this.props.mode === LoginDisplayMode.Register &&
+            <div className="field">
+              <p className="control has-icons-left">
+                <input className="input" type="password" placeholder="Confirm password" />
+                <span className="icon is-small is-left">
+                  <i className="fas fa-lock"></i>
+                </span>
+              </p>
+            </div>
+          }
+        </section>
+
+        <footer className="modal-card-foot">
+          <div className="field">
+            <p className="control">
+              {this.props.mode === LoginDisplayMode.Login &&
+                <button
+                  className="button is-success"
+                  onClick={this.props.login}>
+                    Login
+                </button>
+              }
+
+              {this.props.mode === LoginDisplayMode.Register &&
+                <button
+                  className="button is-success"
+                  onClick={this.props.register}>
+                    Register
+                </button>
+              }
+            </p>
+          </div>
+        </footer>
+      </div>
+    </div>)
+  }
+}
