068e7f2899fa2fa8301839c98c4dad406b9db031
bpglaser <bpglaser@gmail.com>
2019-08-30 21:34:41 -0400

    Fixed burger menu

--- src/BrowserRow.tsx
+++ src/BrowserRow.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import { ArchiveEntry } from "./helpers";
 
-type BrowserRowProps = {
+type Props = {
   active: boolean,
   entry: ArchiveEntry,
   onClickCallback: (entry: ArchiveEntry) => void,
   rowNum: number,
 }
 
-export default class BrowserRow extends React.Component<BrowserRowProps> {
+export default class BrowserRow extends React.Component<Props> {
   render() {
     let entry = this.props.entry
-    return (<tr onMouseOver={() => this.props.onClickCallback(entry)}>
+    let className = this.props.active ? "is-selected" : undefined
+    return (<tr className={className} onMouseOver={() => this.props.onClickCallback(entry)}>
       <td>{entry.name}</td>
       <td>{entry.owner}</td>
       <td>{entry.uploaded.toISOString()}</td>
--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,25 +1,49 @@
-import React from 'react'
+import React from 'react';
 import { Link } from 'react-router-dom';
 
-type Prop = {
-  loggedIn: boolean
-  registerClicked: () => void
-  logInClicked: () => void
-  logOutClicked: () => void
+type Props = {
+  loggedIn: boolean,
+  registerClicked: () => void,
+  logInClicked: () => void,
+  logOutClicked: () => void,
 }
 
-class Navbar extends React.Component<Prop> {
+type State = {
+  burgerState: boolean,
+}
+
+class Navbar extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props)
+    this.state = {
+      burgerState: false,
+    }
+  }
+
   render() {
     return (<div className="navbar">
       <div className="navbar-brand">
         <div className="navbar-item">
           <strong>Robinson Observatory Archive</strong>
         </div>
+
+        <a
+          role="button"
+          className={this.modifyClassName("navbar-burger burger")}
+          aria-label="menu"
+          aria-expanded="false"
+          onClick={this.burgerClicked}>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
       </div>
-      <div className="navbar-menu">
+
+      <div className={this.modifyClassName("navbar-menu")}>
         <div className="navbar-start">
           <Link to="/" className="navbar-item">Home</Link>
         </div>
+
         <div className="navbar-end">
           <div className="navbar-item">
             {!this.props.loggedIn &&
@@ -37,6 +61,19 @@ class Navbar extends React.Component<Prop> {
         </div>
       </div>
     </div>)
+  }
+
+  burgerClicked = () => {
+    this.setState({
+      burgerState: !this.state.burgerState,
+    })
+  }
+
+  modifyClassName = (className: string) => {
+    if (this.state.burgerState) {
+      return className + " is-active"
+    }
+    return className
   }
 }
 
