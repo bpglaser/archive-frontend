37cf68919cafd25ded3057338736ae16989860dc
bpglaser <bpglaser@gmail.com>
2019-09-02 11:19:59 -0400

    Added loading to password update

--- src/Components/Settings/AccountSettings.tsx
+++ src/Components/Settings/AccountSettings.tsx
@@ -1,4 +1,11 @@
 import React from "react";
+import { delay } from "q";
+
+enum Validation {
+  Undetermined,
+  Invalid,
+  Valid,
+}
 
 type PasswordFieldProps = {
   label: string,
@@ -43,25 +50,27 @@ const PasswordField: React.FC<PasswordFieldProps> =
     </div>)
   }
 
-enum Validation {
-  Undetermined,
-  Invalid,
-  Valid,
-}
-
 type State = {
+  awaitingUpdateResponse: boolean,
+  displayMessage: string | null,
+  displayMessageClassNameSuffix: string,
   validation: Validation,
 }
 
 export default class AccountSettings extends React.Component<any, State> {
+  oldPasswordField: React.RefObject<HTMLInputElement>
   newPasswordField: React.RefObject<HTMLInputElement>
   confirmNewPasswordField: React.RefObject<HTMLInputElement>
 
   constructor(props: any) {
     super(props)
+    this.oldPasswordField = React.createRef()
     this.newPasswordField = React.createRef()
     this.confirmNewPasswordField = React.createRef()
     this.state = {
+      awaitingUpdateResponse: false,
+      displayMessage: null,
+      displayMessageClassNameSuffix: "",
       validation: Validation.Undetermined,
     }
   }
@@ -74,7 +83,11 @@ export default class AccountSettings extends React.Component<any, State> {
         <label className="label">Old Password</label>
 
         <div className="control has-icons-left">
-          <input className="input" type="password" placeholder="Old Password" />
+          <input
+            className="input"
+            type="password"
+            placeholder="Old Password"
+            ref={this.oldPasswordField} />
 
           <span className="icon is-small is-left">
             <i className="fas fa-lock"></i>
@@ -93,20 +106,56 @@ export default class AccountSettings extends React.Component<any, State> {
         superRef={this.confirmNewPasswordField}
         onInput={this.newPasswordUpdated}
         validation={this.state.validation} />
+
+      <button
+        className={this.state.awaitingUpdateResponse ? "button is-success is-loading" : "button is-success"}
+        disabled={this.state.awaitingUpdateResponse || this.state.validation !== Validation.Valid}
+        onClick={this.updateButtonOnClick}>
+        Update
+      </button>
+
+      {this.state.displayMessage !== null &&
+        <p className={"help" + this.state.displayMessageClassNameSuffix}>{this.state.displayMessage}</p>
+      }
     </div>)
   }
 
+  validateNewPasswordFields = () => {
+    return (this.newPasswordField.current != null
+      && this.confirmNewPasswordField.current != null
+      && this.newPasswordField.current.value === this.confirmNewPasswordField.current.value)
+  }
+
   newPasswordUpdated = (event: React.FormEvent<HTMLInputElement>) => {
-    if (this.newPasswordField.current != null && this.confirmNewPasswordField.current != null) {
-      if (this.newPasswordField.current.value === this.confirmNewPasswordField.current.value) {
-        this.setState({
-          validation: Validation.Valid
-        })
-      } else {
-        this.setState({
-          validation: Validation.Invalid
-        })
-      }
+    if (this.validateNewPasswordFields()) {
+      this.setState({
+        validation: Validation.Valid
+      })
+    } else {
+      this.setState({
+        validation: Validation.Invalid
+      })
     }
+  }
+
+  updateButtonOnClick = async (event: React.FormEvent<HTMLButtonElement>) => {
+    this.setState({
+      awaitingUpdateResponse: true,
+    })
+
+    await delay(1000)
+
+    if (this.oldPasswordField.current !== null)
+      this.oldPasswordField.current.value = ""
+    if (this.newPasswordField.current !== null)
+      this.newPasswordField.current.value = ""
+    if (this.confirmNewPasswordField.current !== null)
+      this.confirmNewPasswordField.current.value = ""
+
+    this.setState({
+      awaitingUpdateResponse: false,
+      displayMessage: "Password updated successfully!",
+      displayMessageClassNameSuffix: " is-success",
+    })
   }
 }
