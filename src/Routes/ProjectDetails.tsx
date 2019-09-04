705c74f6509337ff414b9eed1d387b335f961ec3
bpglaser <bpglaser@gmail.com>
2019-09-04 14:06:00 -0400

    Added upload prompt

--- src/Routes/ProjectDetails.tsx
+++ src/Routes/ProjectDetails.tsx
@@ -1,15 +1,45 @@
 import React from "react";
 import { RouteComponentProps } from "react-router";
+import UploadPrompt from "../Components/UploadPrompt";
 
 interface Params {
   id: string;
 }
 
-export default class ProjectDetails extends React.Component<RouteComponentProps<Params>> {
+interface State {
+  uploadPromptVisible: boolean;
+}
+
+export default class ProjectDetails extends React.Component<RouteComponentProps<Params>, State> {
+  constructor(props: any) {
+    super(props)
+    this.state = {
+      uploadPromptVisible: false,
+    }
+  }
+
   render() {
     let params = this.props.match.params
     return (<div>
       {params.id}
+      <br></br>
+      <button className="button" onClick={this.showUploadPrompt}>Upload</button>
+
+      {this.state.uploadPromptVisible &&
+        <UploadPrompt close={this.closeUploadPrompt} closeWithSuccess={this.closeUploadPrompt} />
+      }
     </div>)
+  }
+
+  showUploadPrompt = () => {
+    this.setState({
+      uploadPromptVisible: true
+    })
+  }
+
+  closeUploadPrompt = () => {
+    this.setState({
+      uploadPromptVisible: false
+    })
   }
 }
