ce511ee494fe46a0f59dbc3e1ffdab230f5f90c1
bpglaser <bpglaser@gmail.com>
2019-09-11 19:46:18 -0400

    Fixed validation for login/register

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -15,8 +15,11 @@ interface Props {
 }
 
 interface State {
+  emailInvalid: boolean,
   errorMessage: string | null;
   loading: boolean;
+  passwordInvalid: boolean,
+  passwordsMatch: boolean,
 }
 
 export class Login extends React.Component<Props, State> {
@@ -30,8 +33,11 @@ export class Login extends React.Component<Props, State> {
     this.passwordRef = React.createRef();
     this.passwordConfirmationRef = React.createRef();
     this.state = {
+      emailInvalid: false,
       errorMessage: null,
       loading: false,
+      passwordInvalid: false,
+      passwordsMatch: true,
     };
   }
 
@@ -47,9 +53,9 @@ export class Login extends React.Component<Props, State> {
 
         <section className="modal-card-body">
           <div className="field">
-            <p className="control has-icons-left">
+            <p className={this.state.emailInvalid ? "control has-icons-left has-icons-right" : "control has-icons-left"}>
               <input
-                className="input"
+                className={this.state.emailInvalid ? "input is-danger" : "input"}
                 type="email"
                 placeholder="Email"
                 disabled={this.state.loading}
@@ -58,13 +64,23 @@ export class Login extends React.Component<Props, State> {
               <span className="icon is-small is-left">
                 <i className="fas fa-envelope"></i>
               </span>
+
+              {this.state.emailInvalid &&
+                <span className="icon is-small is-right">
+                  <i className="fas fa-exclamation-triangle"></i>
+                </span>
+              }
             </p>
+
+            {this.state.emailInvalid &&
+              <p className="help is-danger">Invalid email address</p>
+            }
           </div>
 
           <div className="field">
-            <p className="control has-icons-left">
+            <p className={this.state.passwordInvalid ? "control has-icons-left has-icons-right" : "control has-icons-left"}>
               <input
-                className="input"
+                className={this.state.passwordInvalid ? "input is-danger" : "input"}
                 type="password"
                 placeholder="Password"
                 disabled={this.state.loading}
@@ -73,14 +89,24 @@ export class Login extends React.Component<Props, State> {
               <span className="icon is-small is-left">
                 <i className="fas fa-lock"></i>
               </span>
+
+              {this.state.passwordInvalid &&
+                <span className="icon is-small is-right">
+                  <i className="fas fa-exclamation-triangle"></i>
+                </span>
+              }
             </p>
+
+            {this.state.passwordInvalid &&
+              <p className="help is-danger">Invalid password</p>
+            }
           </div>
 
           {this.props.mode === LoginDisplayMode.Register &&
             <div className="field">
-              <p className="control has-icons-left">
+              <p className={this.state.passwordsMatch ? "control has-icons-left" : "control has-icons-left has-icons-right"}>
                 <input
-                  className="input"
+                  className={this.state.passwordsMatch ? "input" : "input is-danger"}
                   type="password"
                   placeholder="Confirm password"
                   disabled={this.state.loading}
@@ -89,7 +115,17 @@ export class Login extends React.Component<Props, State> {
                 <span className="icon is-small is-left">
                   <i className="fas fa-lock"></i>
                 </span>
+
+                {!this.state.passwordsMatch &&
+                  <span className="icon is-small is-right">
+                    <i className="fas fa-exclamation-triangle"></i>
+                  </span>
+                }
               </p>
+
+              {!this.state.passwordsMatch &&
+                <p className="help is-danger">Passwords must match</p>
+              }
             </div>
           }
         </section>
@@ -132,22 +168,49 @@ export class Login extends React.Component<Props, State> {
     return s;
   }
 
+  validateFields = (): boolean => {
+    let valid = true;
+
+    if (this.emailRef.current !== null && this.passwordRef.current !== null) {
+      if (vaildEmail(this.emailRef.current.value)) {
+        this.setState({ emailInvalid: false });
+      } else {
+        this.setState({ emailInvalid: true });
+        valid = false;
+      }
+
+      if (validPassword(this.passwordRef.current.value)) {
+        this.setState({ passwordInvalid: false });
+      } else {
+        this.setState({ passwordInvalid: true });
+        valid = false;
+      }
+
+      if (this.props.mode === LoginDisplayMode.Register && this.passwordConfirmationRef.current !== null) {
+        if (this.passwordRef.current.value === this.passwordConfirmationRef.current.value) {
+          this.setState({ passwordsMatch: true });
+        } else {
+          this.setState({ passwordsMatch: false });
+          valid = false;
+        }
+      }
+    }
+
+    return valid;
+  }
+
   login = async () => {
     if (this.emailRef.current !== null && this.passwordRef.current !== null) {
       const email = this.emailRef.current.value;
       const password = this.passwordRef.current.value;
 
-      if (!vaildEmail(email)) {
-        // TODO notify user
-      } else if (!validPassword(password)) {
-        // TODO notify user
-      } else {
+      if (this.validateFields()) {
         this.setState({
           loading: true,
         });
 
         try {
-          const {user, token } = await Backend.login(email, password);
+          const { user, token } = await Backend.login(email, password);
 
           this.setState({
             loading: false,
@@ -171,21 +234,14 @@ export class Login extends React.Component<Props, State> {
     if (this.emailRef.current !== null && this.passwordRef.current !== null && this.passwordConfirmationRef.current !== null) {
       const email = this.emailRef.current.value;
       const password = this.passwordRef.current.value;
-      const passwordConfirmation = this.passwordConfirmationRef.current.value;
 
-      if (!vaildEmail(email)) {
-        // TODO notify user
-      } else if (!validPassword(password)) {
-        // TODO notify user
-      } else if (password !== passwordConfirmation) {
-        // TODO notify user
-      } else {
+      if (this.validateFields()) {
         this.setState({
           loading: true,
         });
 
         try {
-          const {user, token } = await Backend.register(email, password);
+          const { user, token } = await Backend.register(email, password);
 
           this.setState({
             loading: false,
